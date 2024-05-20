c0168a6c1de49e6eb79bd9eec1bc4f27ce2346a8
Ember1998 <sahilm.singh@gmail.com>
2024-05-20 11:18:45 +0545

    SMS: removed demo and weatherforecast

--- AskJavra/Controllers/DemoController.cs
+++ AskJavra/Controllers/DemoController.cs
@@ -1,27 +1,27 @@
-﻿using AskJavra.Models;
-using AskJavra.Repositories;
-using Microsoft.AspNetCore.Mvc;
+﻿//using AskJavra.Models;
+//using AskJavra.Repositories;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace AskJavra.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class DemoController : ControllerBase
-    {
-        private readonly DemoRepository _demoRepo;
-        public DemoController(DemoRepository demoRepo)
-        {
-            _demoRepo = demoRepo;
-        }
-        [HttpGet("DemoMsg")]
-        public IActionResult DemoMsg() {
-            return Ok(_demoRepo.DemoMethod());
-        }
-        [HttpPost("Create")]
-        public IActionResult Create(Demo demo)
-        {
-            _demoRepo.CreateDemo(demo);
-            return Ok();
-        }
-    }
-}
+//namespace AskJavra.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class DemoController : ControllerBase
+//    {
+//        private readonly DemoRepository _demoRepo;
+//        public DemoController(DemoRepository demoRepo)
+//        {
+//            _demoRepo = demoRepo;
+//        }
+//        [HttpGet("DemoMsg")]
+//        public IActionResult DemoMsg() {
+//            return Ok(_demoRepo.DemoMethod());
+//        }
+//        [HttpPost("Create")]
+//        public IActionResult Create(Demo demo)
+//        {
+//            _demoRepo.CreateDemo(demo);
+//            return Ok();
+//        }
+//    }
+//}
