d2f6de60668ed21d569a9e05b3cafd20e0a44de3
Sahil Man Singh <sahil.singh@javra.com>
2024-05-23 17:54:30 +0545

    SMS: done with list

--- AskJavra/ViewModels/UserApiModel.cs
+++ AskJavra/ViewModels/UserApiModel.cs
@@ -7,6 +7,9 @@
         public string UserName { get; set; }
         public string PhoneNumber { get; set; }
         public bool Active { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+
 
     }
 }
