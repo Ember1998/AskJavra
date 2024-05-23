28c7066348caf01b510f548c151cfc75e640bda1
shdha092 <shamser.dhami@javra.com>
2024-05-23 17:39:35 +0545

    Contribution entity migrated to db.

---
+++ AskJavra/Models/Contribution/ContributionRank.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AskJavra.Models.Contribution
+{
+    public class ContributionRank
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public int RankMinPoint { get; set; }
+        public int RankMaxPoint { get;}
+    }
+}
